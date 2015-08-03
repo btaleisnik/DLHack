be56cd048db654cb2c572da5c1cefaa6c25e0b15
Brandon Taleisnik <Brandon@brandons-mbp.attlocal.net>
2015-08-02 20:43:43 -0400

    added typeform and restructured menu bar

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
       get 'signup' => 'signup'
 
+      get 'tools' => 'tools'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
